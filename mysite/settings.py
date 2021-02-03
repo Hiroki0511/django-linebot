dcea2921fc97492799b5d661cc65401e223df61a
Hiroki0511 <ohtahiroki0511@gmail.com>
2021-02-03 15:12:10 +0900

    edit setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,8 +127,4 @@ except ImportError:
 
 if not DEBUG:
     import django_heroku
-    import os
-    CHANNEL_ACCESS_TOKEN = os.environ["CHANNEL_ACCESS_TOKEN"]
-    CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
-    TALK_API = os.environ["TALK_API"]
     django_heroku.settings(locals())
